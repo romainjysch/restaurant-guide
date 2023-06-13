34b4281020e9c54663dce4422e3fba13e0776fd2
Romain Jysch <rjh-network@pm.me>
2023-06-13 07:35:42 +0200

    refactor(restaurant-guide): Error logs configuration

--- src/main/java/ch/romainjysch/restaurantguide/application/Main.java
+++ src/main/java/ch/romainjysch/restaurantguide/application/Main.java
@@ -1,22 +1,29 @@
 package ch.romainjysch.restaurantguide.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.romainjysch.restaurantguide.persistence.CityDAO;
 import ch.romainjysch.restaurantguide.persistence.Database;
 import ch.romainjysch.restaurantguide.service.RestaurantService;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+
     public static void main(String[] args) {
         cli();
     }
 
     private static void cli() {
+        logger.info("Main # CLI Start");
         try (var database = Database.getInstance()) {
             RestaurantService restaurantService = RestaurantService.getInstance(
                     database,
                     CityDAO.getInstance());
             System.out.println(restaurantService.researchCityById(1));
         }
+        logger.info("Main # CLI Close");
     }
 
 }
--- src/main/java/ch/romainjysch/restaurantguide/persistence/Database.java
+++ src/main/java/ch/romainjysch/restaurantguide/persistence/Database.java
@@ -27,35 +27,33 @@ public class Database implements AutoCloseable {
     }
 
     public <T> T inTransaction(Supplier<T> function) {
-        logger.info("Database # Create EntityManager");
         EntityManager em = emf.createEntityManager();
         try {
             Database.em.set(em);
-            logger.info("Database # Begin transaction");
             em.getTransaction().begin();
             T t = function.get();
-            logger.info("Database # Commit transaction");
             em.getTransaction().commit();
             return t;
+        } catch (Exception e) {
+            logger.error("Exception : ", e);
+            throw new RuntimeException(e);
         } finally {
-            logger.info("Database # Close EntityManager");
             //em.close();
             //Database.em.remove();
         }
     }
 
     public void inTransaction(Runnable function) {
-        logger.info("Database # Start inTransaction");
         EntityManager em = emf.createEntityManager();
         try {
             Database.em.set(em);
-            logger.info("Database # Begin transaction");
             em.getTransaction().begin();
             function.run();
-            logger.info("Database # Commit transaction");
             em.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Exception : ", e);
+            throw new RuntimeException(e);
         } finally {
-            logger.info("Database # Close EntityManager");
             //em.close();
             //Database.em.remove();
         }
